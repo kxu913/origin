d775dc128532c54cd601af9f321ff643410e159d
admin <kevin_913@qq.com>
2024-01-02 10:07:39 +0800

    [feature]: add file module.

---
+++ origin-framework-file/src/main/java/com/origin/framework/file/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.origin.framework.file.constants;
+
+public final class Constants {
+
+    public static final String DELIMITER = "\n";
+    public static final String ENCODE = "UTF-8";
+}
