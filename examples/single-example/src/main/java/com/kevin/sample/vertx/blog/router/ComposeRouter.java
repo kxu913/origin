10ec457c6f18f5902d5410a20d1f51dd517929f5
kxu913 <kxu913@gmail.com>
2024-01-03 15:32:29 +0800

    [refactor]: update example that use mulitiple datasource from one row to mulitiple rows.

--- examples/single-example/src/main/java/com/kevin/sample/vertx/blog/router/ComposeRouter.java
+++ examples/single-example/src/main/java/com/kevin/sample/vertx/blog/router/ComposeRouter.java
@@ -7,9 +7,11 @@ import com.origin.starter.web.OriginWebApplication;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowIterator;
 import io.vertx.sqlclient.RowSet;
 import io.vertx.sqlclient.SqlClient;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,19 +24,27 @@ public class ComposeRouter implements OriginRouter {
                     SqlClient remoteSqlClient = OriginWebApplication.getBeanFactory().getSqlClient("remote-db");
                     SqlClient sqlClient = OriginWebApplication.getBeanFactory().getSqlClient();
                     JsonObject customJsonObject = originConfig.getAppConfig().getJsonObject("custom-config");
-                    Future<RowSet<Row>> blogFuture = sqlClient.preparedQuery("select * from blog limit 1").execute();
-                    Future<RowSet<Row>> blog2Future = remoteSqlClient.preparedQuery("select * from blog limit 1").execute();
+                    Future<RowSet<Row>> blogFuture = sqlClient.preparedQuery("select * from blog limit 10").execute();
+                    Future<RowSet<Row>> blog2Future = remoteSqlClient.preparedQuery("select * from blog_detail limit 10").execute();
                     Future.all(blogFuture, blog2Future)
                             .onSuccess(ar -> {
+                                // here is just a sample that combine columns from different datasource.
+                                // actually you also can compose data from different datasource.
                                 List<RowSet<Row>> list = ar.list();
-                                Map<String, Object> result = new HashMap<>();
-                                list.forEach(rowset -> {
-                                    rowset.forEach(row -> {
-                                        JsonObject blog = row.toJson();
-                                        result.putAll(blog.getMap());
-                                    });
-                                });
-                                result.putAll(customJsonObject.getMap());
+                                RowSet<Row> blogRowSet = list.get(0);
+                                RowIterator<Row> it = blogRowSet.iterator();
+                                RowSet<Row> blog2RowSet = list.get(1);
+                                RowIterator<Row> it2 = blog2RowSet.iterator();
+                                List<Map<String, Object>> result = new ArrayList<>(blogRowSet.size());
+
+                                for (int i = 0; i < blogRowSet.size(); i++) {
+                                    Map<String, Object> row = new HashMap<>();
+                                    row.putAll(it.next().toJson().getMap());
+                                    row.putAll(it2.next().toJson().getMap());
+                                    row.putAll(customJsonObject.getMap());
+                                    result.add(row);
+                                }
+
                                 ctx.json(result);
                                 sqlClient.close();
                                 remoteSqlClient.close();
