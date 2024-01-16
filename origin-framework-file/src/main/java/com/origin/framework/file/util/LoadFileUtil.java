36a960950afbdbd88a24ee8b68f5d2c15f933bf6
kxu913 <kxu913@gmail.com>
2024-01-16 16:34:41 +0800

    [feature]: support load file to redis using batch mode.

--- origin-framework-file/src/main/java/com/origin/framework/file/util/LoadFileUtil.java
+++ origin-framework-file/src/main/java/com/origin/framework/file/util/LoadFileUtil.java
@@ -10,10 +10,16 @@ import io.vertx.core.file.AsyncFile;
 import io.vertx.core.file.OpenOptions;
 import io.vertx.core.parsetools.RecordParser;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.redis.client.Command;
+import io.vertx.redis.client.RedisConnection;
 import io.vertx.redis.client.Request;
+import io.vertx.redis.client.Response;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 /**
@@ -36,23 +42,31 @@ public class LoadFileUtil {
                                 RoutingContext ctx,
                                 LoadFileRequest request,
                                 Function<String, Request> fn) {
-        Optional<String> file = Optional.of(request.getFile());
-        ResultReport resultReport = new ResultReport().start();
-        if (log.isDebugEnabled()) {
-            log.debug("receive a request {} that load file to redis.", request);
-        }
-        Future<AsyncFile> asyncFileFuture = originVertxContext.getFs().open(file.get(), new OpenOptions());
-        AsyncResultHandler.handleFuture(asyncFileFuture, ctx, asyncFile -> {
+        try {
+            Optional<String> file = Optional.of(request.getFile());
+            ResultReport resultReport = new ResultReport().start();
+            if (log.isDebugEnabled()) {
+                log.debug("receive a request {} that load file to redis.", request);
+            }
+            Future<AsyncFile> asyncFileFuture = originVertxContext.getFs().open(file.get(), new OpenOptions());
+            AsyncResultHandler.handleFuture(asyncFileFuture, ctx, asyncFile -> {
 
-            asyncFile.handler(createRecordParser(request, fn, resultReport)).endHandler(v -> {
-                asyncFile.close();
-                request.getConnection().close();
-                ctx.json(resultReport);
+                asyncFile.handler(createRecordParser(request, fn, resultReport)).endHandler(v -> {
+                    asyncFile.close();
+                    request.getConnection().close();
+                    ctx.json(resultReport);
+
+                });
+
 
             });
+        } catch (NullPointerException ne) {
+            ctx.fail(400, ne);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            ctx.fail(500, e);
+        }
 
-
-        });
 
     }
 
@@ -66,29 +80,192 @@ public class LoadFileUtil {
     public static void loadFile(OriginWebVertxContext originVertxContext,
                                 LoadFileRequest request,
                                 Function<String, Request> fn) {
-        Optional<String> file = Optional.of(request.getFile());
-        ResultReport resultReport = new ResultReport().start();
-        if (log.isDebugEnabled()) {
-            log.debug("receive a request {} that load file to redis.", request);
-        }
-        Future<AsyncFile> asyncFileFuture = originVertxContext.getFs().open(file.get(), new OpenOptions());
-        AsyncResultHandler.handleFuture(asyncFileFuture, asyncFile -> {
+        try {
+            Optional<String> file = Optional.of(request.getFile());
 
-            asyncFile.handler(createRecordParser(request, fn, resultReport)).endHandler(v -> {
-                asyncFile.close();
-                request.getConnection().close();
-                log.info("statistics info is {}", resultReport);
+            ResultReport resultReport = new ResultReport().start();
+            if (log.isDebugEnabled()) {
+                log.debug("receive a request {} that load file to redis.", request);
+            }
+            Future<AsyncFile> asyncFileFuture = originVertxContext.getFs().open(file.get(), new OpenOptions());
+            AsyncResultHandler.handleFuture(asyncFileFuture, asyncFile -> {
+
+                asyncFile.handler(createRecordParser(request, fn, resultReport)).endHandler(v -> {
+                    asyncFile.close();
+                    request.getConnection().close();
+                    log.info("statistics info is {}", resultReport);
+
+                });
+
 
             });
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
 
+
+    }
+
+    public static void batchLoadFile(OriginWebVertxContext originVertxContext,
+                                     RoutingContext ctx,
+                                     LoadFileRequest request,
+                                     Function<String, String> fn) {
+        try {
+            Optional<String> file = Optional.of(request.getFile());
+            Optional<String> redisIndex = Optional.of(request.getIndex());
+
+            ResultReport resultReport = new ResultReport().start();
+            if (log.isDebugEnabled()) {
+                log.debug("receive a request {} that load file to redis.", request);
+            }
+            Future<AsyncFile> asyncFileFuture = originVertxContext.getFs().open(file.get(), new OpenOptions());
+            List<Future<Response>> futureList = new ArrayList<>();
+            AsyncResultHandler.handleFuture(asyncFileFuture, ctx, asyncFile -> {
+                Optional<RedisConnection> connection = Optional.of(request.getConnection());
+                AtomicInteger batchSize = new AtomicInteger(10000);
+                AtomicInteger cursor = new AtomicInteger(0);
+                List<String> keyList = new ArrayList<>(batchSize.get());
+                RecordParser recordParser = createRecordParserForBatch(request, fn, cursor, batchSize, keyList, futureList, resultReport);
+
+                asyncFile.handler(recordParser).endHandler(v -> {
+                    asyncFile.close();
+                    if (!keyList.isEmpty()) {
+                        futureList.add(createBatchRedisRequest(connection.get(), redisIndex.get(), keyList));
+                    }
+                    if (log.isDebugEnabled()) {
+                        log.debug("file {} had been split to {} redis batch request, each batch size is {}", request.getFile(), futureList.size(), batchSize);
+                    }
+                    Future.all(futureList)
+                            .onSuccess(ar -> {
+                                List<Response> rList = ar.list();
+                                if (log.isDebugEnabled()) {
+                                    log.debug("received {} success redis response.", rList.size());
+                                }
+
+                                rList.forEach(r -> resultReport.getLoadedSize().addAndGet(r.toInteger()));
+                                request.getConnection().close();
+                                resultReport.end();
+                                log.info("statistics info is {}", resultReport);
+                                ctx.json(resultReport);
+                            }).onFailure(err -> {
+                                log.error(err.getMessage(), err);
+                                request.getConnection().close();
+                            });
+
+
+                });
+
+
+            });
+        } catch (NullPointerException ne) {
+            ctx.fail(400, ne);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            ctx.fail(500, e);
+        }
+
+
+    }
+
+    public static void batchLoadFile(OriginWebVertxContext originVertxContext,
+                                     LoadFileRequest request,
+                                     Function<String, String> fn) {
+        try {
+            Optional<String> file = Optional.of(request.getFile());
+            Optional<String> redisIndex = Optional.of(request.getIndex());
+            Optional<RedisConnection> connection = Optional.of(request.getConnection());
+            ResultReport resultReport = new ResultReport().start();
+            if (log.isDebugEnabled()) {
+                log.debug("receive a request {} that load file to redis.", request);
+            }
+            Future<AsyncFile> asyncFileFuture = originVertxContext.getFs().open(file.get(), new OpenOptions());
+            List<Future<Response>> futureList = new ArrayList<>();
+            AsyncResultHandler.handleFuture(asyncFileFuture, asyncFile -> {
+
+                AtomicInteger batchSize = new AtomicInteger(10000);
+                AtomicInteger cursor = new AtomicInteger(0);
+                List<String> keyList = new ArrayList<>(batchSize.get());
+                RecordParser recordParser = createRecordParserForBatch(request, fn, cursor, batchSize, keyList, futureList, resultReport);
+
+                asyncFile.handler(recordParser).endHandler(v -> {
+
+                    asyncFile.close();
+                    if (!keyList.isEmpty()) {
+                        futureList.add(createBatchRedisRequest(connection.get(), redisIndex.get(), keyList));
+                    }
+                    Future.all(futureList)
+                            .onSuccess(ar -> {
+                                List<Response> rList = ar.list();
+                                rList.forEach(r -> {
+                                    resultReport.getLoadedSize().addAndGet(r.toInteger());
+                                });
+                                request.getConnection().close();
+                                resultReport.end();
+                                log.info("statistics info is {}", resultReport);
+                            }).onFailure(err -> {
+                                log.error(err.getMessage(), err);
+                                request.getConnection().close();
+                            });
+
+
+                });
+
+
+            });
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+
+
+    }
+
+    private static RecordParser createRecordParserForBatch(LoadFileRequest request,
+                                                           Function<String, String> fn,
+                                                           AtomicInteger cursor,
+                                                           AtomicInteger batchSize,
+                                                           List<String> keyList,
+                                                           List<Future<Response>> futureList,
+                                                           ResultReport resultReport) {
+        return RecordParser.newDelimited(request.getDelimiter(), bufferLine -> {
+            String line = bufferLine.toString(request.getEncode());
+            if (log.isDebugEnabled()) {
+                log.debug("line: {}", line);
+            }
+            try {
+                String key = fn.apply(line);
+                if (cursor.incrementAndGet() <= batchSize.get()) {
+                    keyList.add(key.trim()
+                            .replaceAll("\r", "")
+                            .replaceAll("\n", ""));
+                } else {
+                    futureList.add(createBatchRedisRequest(request.getConnection(), request.getIndex(), keyList));
+                    cursor.set(0);
+                    keyList.clear();
+                }
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                resultReport.getErrorSize().incrementAndGet();
+            }
+            resultReport.getTotalSize().incrementAndGet();
 
         });
+    }
 
+
+    private static Future<Response> createBatchRedisRequest(RedisConnection connection, String redisIndex, List<String> keyList) {
+        Request redisRequest = Request.cmd(Command.SADD)
+                .arg(redisIndex);
+        keyList.forEach(redisRequest::arg);
+
+        return connection.send(redisRequest);
     }
 
 
     private static RecordParser createRecordParser(LoadFileRequest request, Function<String, Request> fn, ResultReport resultReport) {
+        Optional<RedisConnection> connection = Optional.of(request.getConnection());
+
         return RecordParser.newDelimited(request.getDelimiter(), bufferLine -> {
+
             String line = bufferLine.toString(request.getEncode());
             if (log.isDebugEnabled()) {
                 log.debug("line: {}", line);
@@ -97,7 +274,7 @@ public class LoadFileUtil {
                 try {
                     Request redisRequest = fn.apply(line);
                     if (redisRequest != null) {
-                        request.getConnection().send(fn.apply(line))
+                        connection.get().send(fn.apply(line))
                                 .onSuccess(ar -> resultReport.getLoadedSize().incrementAndGet())
                                 .onFailure(e -> resultReport.getErrorSize().incrementAndGet());
                     }
@@ -109,7 +286,7 @@ public class LoadFileUtil {
 
             } else {
                 try {
-                    request.getConnection().send(fn.apply(line))
+                    connection.get().send(fn.apply(line))
                             .onSuccess(ar -> resultReport.getLoadedSize().incrementAndGet())
                             .onFailure(e -> resultReport.getErrorSize().incrementAndGet());
                 } catch (Exception e) {
@@ -122,5 +299,6 @@ public class LoadFileUtil {
 
         });
     }
+
 
 }
